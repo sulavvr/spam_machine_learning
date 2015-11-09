3cbda46b072bef9b80e46c49416e3e76bc194147
Sulav Regmi <sulav@ITS078594-M.local>
2015-11-09 16:43:26 -0600

    k k khai k k

--- hw05.py
+++ hw05.py
@@ -1,20 +1,25 @@
-import numpy as np
+# import numpy as np
 from sklearn import svm
+from sklearn.datasets import load_svmlight_file
 import os
 import re
+import collections
 
 
 def read_files(target, dictionary):
     i = 1
     folders = os.listdir()
+    ordered_spam = []
+    ordered_nspam = []
     for folder in folders:
         # spams
         if folder == "spam":
             spams = os.listdir("spam")
             for spam in spams:
+                spam_dict = {}
                 with open(folder+"/"+spam, "r", encoding="ISO-8859-1") as f:
                     content = f.read()
-                    target.write("1 ")
+                    # target.write("1 ")
                     for line in content.splitlines():
                         if line != "":
                             for word in re.split(",|\"| |\.", line):
@@ -22,20 +27,25 @@ def read_files(target, dictionary):
                                 if mword != '':
                                     if mword not in dictionary.values():
                                         dictionary[i] = mword
-                                        target.write(str(i) + ": 1 ")
+                                        spam_dict[i] = 1
+                                        # target.write(str(i) + ":1 ")
                                         i += 1
                                     else:
                                         k = check_dictionary(dictionary, mword)
                                         if (k):
-                                            target.write(str(k) + ": 1 ")
-                    target.write("\n")
+                                            spam_dict[k] = 1
+                                            # target.write(str(k) + ":1 ")
+
+                    ordered_spam.append(collections.OrderedDict(sorted(spam_dict.items())))
+                    # target.write("\n")
         # not spams
         if folder == "nspam":
             nspams = os.listdir("nspam")
             for nspam in nspams:
+                nspam_dict = {}
                 with open(folder+"/"+nspam, "r", encoding="ISO-8859-1") as f:
                     content = f.read()
-                    target.write("0 ")
+                    # target.write("0 ")
                     for line in content.splitlines():
                         if line != " ":
                             for word in re.split(",|\"| |\.", line):
@@ -43,14 +53,17 @@ def read_files(target, dictionary):
                                 if mword != '':
                                     if mword not in dictionary.values():
                                         dictionary[i] = mword
-                                        target.write(str(i) + ": 1 ")
+                                        nspam_dict[i] = 1
+                                        # target.write(str(i) + ":1 ")
                                         i += 1
                                     else:
                                         k = check_dictionary(dictionary, mword)
                                         if (k):
-                                            target.write(str(k) + ": 1 ")
-                    target.write("\n")
-    return dictionary
+                                            nspam_dict[k] = 1
+                                            # target.write(str(k) + ":1 ")
+                    ordered_nspam.append(collections.OrderedDict(sorted(nspam_dict.items())))
+                    # target.write("\n")
+    return [ordered_spam, ordered_nspam]
 
 
 def check_dictionary(dictionary, word):
@@ -64,12 +77,26 @@ def check_dictionary(dictionary, word):
 target = open("test.txt", "w")
 dictionary = {}
 x = read_files(target, dictionary)
-# dictionary_text = open("dictionary.txt", "w")
-# dictionary_text.write(str(x))
-# dictionary_text.close()
+
+#spam
+for arr in x[0]:
+    target.write("-1 ")
+    for key, value in arr.items():
+        target.write(str(key) + ":" + str(value) + " ")
+    target.write("")
+
+# not spam
+for arr in x[1]:
+    target.write("1 ")
+    for key, value in arr.items():
+        target.write(str(key) + ":" + str(value) + " ")
+    target.write("")
+
 target.close()
 C = 1.0  # SVM regularization parameter
-svc = svm.SVC(kernel='linear', C=C)
+data = load_svmlight_file("test.txt")
+print(data[0])
+# svc = svm.SVC(kernel='linear', C=C)
 
-y_predict = svc.predict(open("test.txt", "r").read())
-print(y_predict)
+# y_predict = svc.predict(open("test.txt", "r").read())
+# print(y_predict)
