70de956ea2baa4d80b9d021831b9817136369171
Sulav Regmi <sulavvr@gmail.com>
2015-11-09 23:59:04 -0600

    trying with test data

--- hw05.py
+++ hw05.py
@@ -6,11 +6,11 @@ import re
 import collections
 
 
-def read_files(target, dictionary):
+def read_files():
+    dictionary = {}
     i = 1
     folders = os.listdir()
-    ordered_spam = []
-    ordered_nspam = []
+    ordered_spam, ordered_nspam = [], []
     for folder in folders:
         # spams
         if folder == "spam":
@@ -32,12 +32,9 @@ def read_files(target, dictionary):
                                         i += 1
                                     else:
                                         k = check_dictionary(dictionary, mword)
-                                        if (k):
+                                        if k:
                                             spam_dict[k] = 1
-                                            # target.write(str(k) + ":1 ")
-
                     ordered_spam.append(collections.OrderedDict(sorted(spam_dict.items())))
-                    # target.write("\n")
         # not spams
         if folder == "nspam":
             nspams = os.listdir("nspam")
@@ -45,7 +42,6 @@ def read_files(target, dictionary):
                 nspam_dict = {}
                 with open(folder+"/"+nspam, "r", encoding="ISO-8859-1") as f:
                     content = f.read()
-                    # target.write("0 ")
                     for line in content.splitlines():
                         if line != " ":
                             for word in re.split(",|\"| |\.", line):
@@ -54,31 +50,26 @@ def read_files(target, dictionary):
                                     if mword not in dictionary.values():
                                         dictionary[i] = mword
                                         nspam_dict[i] = 1
-                                        # target.write(str(i) + ":1 ")
                                         i += 1
                                     else:
                                         k = check_dictionary(dictionary, mword)
-                                        if (k):
+                                        if k:
                                             nspam_dict[k] = 1
-                                            # target.write(str(k) + ":1 ")
                     ordered_nspam.append(collections.OrderedDict(sorted(nspam_dict.items())))
-                    # target.write("\n")
     return [ordered_spam, ordered_nspam]
 
 
-def check_dictionary(dictionary, word):
-    for key in dictionary.keys():
-        if (word == dictionary[key]):
-            return key
+def check_dictionary(word_dictionary, word):
+    for k in word_dictionary.keys():
+        if word == word_dictionary[k]:
+            return k
 
     return False
 
-
 target = open("test.txt", "w")
-dictionary = {}
-x = read_files(target, dictionary)
+x = read_files()
 
-#spam
+# spam
 for arr in x[0]:
     target.write("-1 ")
     for key, value in arr.items():
@@ -93,9 +84,10 @@ for arr in x[1]:
     target.write("\n")
 
 target.close()
+
 C = 1.0  # SVM regularization parameter
 data = load_svmlight_file("test.txt")
 svc = svm.SVC(kernel='linear', C=C)
-print(svc.fit(data[0], data[1]))
-# y_predict = svc.predict()
-# print(y_predict)
+svc.fit(data[0], data[1])
+y_predict = svc.predict(data[0])
+print(y_predict)
